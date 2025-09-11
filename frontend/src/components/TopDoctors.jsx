3a7a1f58a50a32fc29254c2d10f4e0158c00394b
vishalprajapati15 <vishalprajapati3266@gmail.com>
2025-09-11 14:21:24 +0530

    Doctor page logic added

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -12,10 +12,10 @@ const TopDoctors = () => {
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
         <p className='sm:w-1/3 text-center text-sm '>Simply browse through our extensive list of trusted doctors.</p>
-        <div className='w-full grid grid-cols-auto my-grid gap-4 pt-5  gap-y-5 px-3 sm:px-0'>{/* slice for small data from a large dataset here 12 doctors from all doctors list     */}
-            {doctors.slice(0,12).map((item, index)=>(
+        <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-5  gap-y-5 px-3 sm:px-0'>{/* slice for small data from a large dataset here 12 doctors from all doctors list     */}
+            {doctors.slice(0,10).map((item, index)=>(
                 <div onClick={()=>navigate(`/appointment/${item._id}`)} key={index} className=' border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
-                    <img src={item.image} alt="" className='bg-blue-50 '/>
+                    <img src={item.image} alt="" className='bg-blue-50 hover:scale-105 transition-all duration-500'/>
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                             <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Availabe</p>
